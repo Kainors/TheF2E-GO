81d00da502d4e6f75b482b833158d0a0848f623a
Kainors <landroval.lin@gmail.com>
2018-06-04 18:54:41 +0800

    week 1 first commit

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 8888;
 
 app.use(express.static(path.join(__dirname, './public')));
+app.use('/design', express.static(path.join(__dirname, './design')));
 
 app.listen(PORT, () => {
   console.log(`Server listen at ${PORT}`);
